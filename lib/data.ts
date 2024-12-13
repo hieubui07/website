69623d02ce78694ed76afc69175427f27e29c92f
hieubui07 <111406586+hieubui07@users.noreply.github.com>
2024-12-12 20:01:12 -0600

    working navbar

---
+++ lib/data.ts
@@ -0,0 +1,18 @@
+
+export const links = [
+      {
+        href: '/projects',
+        key: 'projects',
+        label: "Projects",
+      },
+      {
+        href: '/',
+        key: 'experience',
+        label: "Experience",
+      },
+      {
+        href: '/',
+        key: 'contact',
+        label: "Contact",
+      },
+];
