7f01db6a572b97e887b8cea6399f3b7e521e6209
hieubui07 <111406586+hieubui07@users.noreply.github.com>
2025-02-24 10:05:06 -0600

    remove background

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,11 @@
 "use client";
-import ShootingStars from "@/app/components/ui/shooting-stars";
-import StarsBackground from "@/app/components/ui/stars-background";
 import Navbar from "@/app/components/Navbar";
 
 export default function Header() {
   return (
     <>
       <div className="absolute top-0 left-0 w-full h-full z-[0]">
-        <ShootingStars />
-        <StarsBackground />
+       
       </div>
       <Navbar />
     </>
